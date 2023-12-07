8fb4b6c34d3f8e73e5dceb2292a5e1e7aef657e0
szmisztal <sz.misztal@gmail.com>
2023-12-07 17:24:43 +0100

    refactor whole old code, now I have only 1 table and work on it

--- db_utils.py
+++ db_utils.py
@@ -5,7 +5,7 @@ from sqlite3 import Error
 class SQLite:
     def __init__(self, db_file):
         self.db_file = db_file
-        self.create_db_tables()
+        self.create_tables()
 
     def create_connection(self):
         try:
@@ -17,59 +17,63 @@ class SQLite:
 
     def execute_sql_query(self, query, *args, fetch_option = None):
         connection = self.create_connection()
-        cursor = connection.cursor()
         if connection is not None:
-            try:
-                cursor.execute(query, *args)
-                connection.commit()
-                if fetch_option == "fetchone":
-                    return cursor.fetchone()
-                elif fetch_option == "fetchall":
-                    return cursor.fetchall()
-            except Error as e:
-                print(f"Error: {e}")
-                connection.rollback()
-            finally:
-                cursor.close()
-                connection.close()
+            with connection:
+                cursor = connection.cursor()
+                try:
+                    cursor.execute(query, *args)
+                    connection.commit()
+                    if fetch_option == "fetchone":
+                        return cursor.fetchone()
+                    elif fetch_option == "fetchall":
+                        return cursor.fetchall()
+                except Error as e:
+                    print(f"Error: {e}")
+                    connection.rollback()
         else:
             print("Cannot create the database connection.")
 
-    def create_year_table(self):
-        query = """ CREATE TABLE IF NOT EXISTS years(
-                        year_id INTEGER PRIMARY KEY,
-                        year INTEGER NOT NULL
-                        ); """
+    def create_day_detail_table(self):
+        query = """CREATE TABLE IF NOT EXISTS day_details(
+                    date_id INTEGER PRIMARY KEY,
+                    day INTEGER NOT NULL,
+                    month INTEGER NOT NULL,
+                    year INTEGER NOT NULL,
+                    start VARCHAR NOT NULL,
+                    end VARCHAR NOT NULL,
+                    hours VARCHAR NOT NULL,
+                    kilometers INTEGER NOT NULL,
+                    refuel INTEGER NOT NULL,
+                    standard_fuel_usage INTEGER NOT NULL,
+                    fuel_difference INTEGER NOT NULL
+                    );"""
         self.execute_sql_query(query)
 
-    def create_month_table(self):
-        query = """ CREATE TABLE IF NOT EXISTS months(
-                        month_id INTEGER PRIMARY KEY,
-                        month INTEGER NOT NULL,
-                        year INTEGER NOT NULL,
-                        FOREIGN KEY (year) REFERENCES years (year_id) ON DELETE PROTECT
-                        ); """
-        self.execute_sql_query(query)
+    # def create_salary_table(self):
+    #     query = """CREATE TABLE IF NOT EXISTS salaries(
+    #                 salary_id INTEGER PRIMARY KEY,
+    #                 month INTEGER NOT NULL,
+    #                 year INTEGER NOT NULL,
+    #                 amount REAL NOT NULL,
+    #                 FOREIGN KEY (month) REFERENCES dates (month),
+    #                 FOREIGN KEY (year) REFERENCES dates (year)
+    #                 );"""
+    #     self.execute_sql_query(query)
 
-    def create_day_details_table(self):
-        query = """ CREATE TABLE IF NOT EXISTS days(
-                        day_id INTEGER PRIMARY KEY,
-                        month INTEGER NOT NULL,
-                        year INTEGER NOT NULL,
-                        months_day INTEGER NOT NULL,
-                        start_hour TEXT NOT NULL,
-                        end_hour TEXT NOT NULL,
-                        work_hours TEXT NOT NULL,
-                        kilometers INTEGER NOT NULL,
-                        liters_refueled INTEGER NOT NULL,
-                        fuel_consumption_by_standard INTEGER NOT NULL,
-                        status_of_fuel_standard INTEGER NOT NULL,                       
-                        FOREIGN KEY (month) REFERENCES months (month_id) ON DELETE PROTECT,
-                        FOREIGN KEY (year) REFERENCES years (year_id) ON DELETE PROTECT
-                        ); """
-        self.execute_sql_query(query)
+    def create_tables(self):
+        self.create_day_detail_table()
+        # self.create_salary_table()
 
-    def create_db_tables(self):
-        self.create_year_table()
-        self.create_month_table()
-        self.create_day_details_table()
+    def add_day_detail(self, day, month, year, start, end, hours, kilometers, refuel,
+                       standard_fuel_usage, fuel_difference):
+        query = "INSERT INTO day_details (day, month, year, start, end, hours, " \
+                "kilometers, refuel, standard_fuel_usage, fuel_difference) " \
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        self.execute_sql_query(query, (day, month, year, start, end, hours, kilometers, refuel,
+                                       standard_fuel_usage, fuel_difference))
+
+    def show_all_days(self):
+        query = "SELECT * FROM day_details ORDER BY day, month, year"
+        result = self.execute_sql_query(query, fetch_option = "fetchall")
+        return result
+
