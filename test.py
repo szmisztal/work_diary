f5404129f576f94b88cd0fbd79d57a5e6ce34389
szmisztal <sz.misztal@gmail.com>
2023-12-07 17:23:36 +0100

    my tests

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+def calculate_fuel_difference(refuel, standard_fuel_usage, actual_fuel_difference):
+    difference = standard_fuel_usage - refuel
+    difference_to_save_in_db = actual_fuel_difference + difference
+    return difference_to_save_in_db
 
+print(calculate_fuel_difference(30, 22, 10))
