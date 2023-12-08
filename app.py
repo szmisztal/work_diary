7b60caad7089f932a0811117e3402881c40bd344
szmisztal <sz.misztal@gmail.com>
2023-12-08 17:19:31 +0100

    add all functions to servic with monthly details

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from db_utils import SQLite
-from config_variables import db_file, month, year
+from config_variables import db_file, month, year, daily_rate, hourly_rate
 
 
 class App:
@@ -8,9 +8,10 @@ class App:
         self.db = SQLite(db_file)
         self.is_running = False
         self.commands = {
-            "1": "Add new day details",
-            "2": "Show all days",
-            "3": "Stop app"
+            "1": "Add new daily summary",
+            "2": "Show monthly summary by days",
+            "3": "Show monthly summary",
+            "4": "Stop app"
         }
 
     def read_dict(self, dict):
@@ -21,11 +22,45 @@ class App:
 
     def days_table_template(self, days_details):
         print("--------")
-        print("DATE:\t\tSTART HOUR:\t\tEND HOUR:\t\tTOTAL HOURS:\t\tKILOMETERS:\t\tREFUEL:\t\tFUELS STANDARD:\t\tDIFFERENCE:")
-        for date in days_details:
-            print(f"{date[1]}-{date[2]}-{date[3]}\t{date[4]}\t\t\t{date[5]}\t\t\t{date[6]}\t\t\t\t{date[7]}\t\t\t\t{date[8]}\t\t\t{date[9]}\t\t\t\t\t{date[10]}")
+        print("{:<12} {:<16} {:<16} {:<18} {:<14} {:<9} {:<18} {:<12}".format(
+            "DATE:", "START HOUR:", "END HOUR:", "HOURS:", "KILOMETERS:", "REFUEL:", "FUELS STANDARD:", "DIFFERENCE:"
+        ))
+        for data in days_details:
+            formatted_data = f"{data[1]}-{data[2]}-{data[3]}"
+            formatted_start_hour = data[4]
+            formatted_end_hour = data[5]
+            formatted_total_hours = data[6]
+            formatted_kilometers = data[7]
+            formatted_refuel = data[8]
+            formatted_fuels_standard = data[9]
+            formatted_difference = data[10]
+            print("{:<12} {:<16} {:<16} {:<18} {:<14} {:<9} {:<18} {:<12}".format(
+                formatted_data, formatted_start_hour, formatted_end_hour, formatted_total_hours, formatted_kilometers,
+                formatted_refuel, formatted_fuels_standard, formatted_difference
+            ))
 
-    def add_new_detail(self):
+    def month_table_template(self, month_details):
+        print("--------")
+        print("{:<8} {:<7} {:<15} {:<14} {:<24} {:<13} {:<9} {:<17} {:<13} {:<10}".format(
+            "MONTH:", "YEAR:", "WORKING DAYS:", "TOTAL HOURS:", "AVERAGE HOURS PER DAY:", "KILOMETERS:", "REFUEL:",
+            "FUELS STANDARD:", "DIFFERENCE:", "SALARY:"
+        ))
+        formatted_month = month_details[1]
+        formatted_year = month_details[2]
+        formatted_working_days = month_details[3]
+        formatted_total_hours = month_details[4]
+        formatted_average_hours = month_details[5]
+        formatted_kilometers = month_details[6]
+        formatted_refuel = month_details[7]
+        formatted_fuels_standard = month_details[8]
+        formatted_difference = month_details[9]
+        formatted_salary = month_details[10]
+        print("{:<8} {:<7} {:<15} {:<14} {:<24} {:<13} {:<9} {:<17} {:<13} {:<10}".format(
+            formatted_month, formatted_year, formatted_working_days, formatted_total_hours, formatted_average_hours,
+            formatted_kilometers, formatted_refuel, formatted_fuels_standard, formatted_difference, formatted_salary
+            ))
+
+    def add_new_daily_summary(self):
         try:
             day = int(input("DAY (number): "))
             start = input("START HOUR (H:M format): ")
@@ -35,13 +70,41 @@ class App:
             refuel = int(input("REFUEL: "))
             standard_fuel_usage = self.calculate_standard_fuel_usage(kilometers)
             fuel_difference = self.calculate_fuel_difference(refuel, standard_fuel_usage)
-            self.db.add_day_detail(day, month, year, start, end, hours, kilometers, refuel, standard_fuel_usage, fuel_difference)
+            self.db.add_daily_summary(day, month, year, start, end, hours, kilometers, refuel, standard_fuel_usage, fuel_difference)
         except Exception as e:
             print(e)
 
-    def show_all_days_details(self):
-        days_details = self.db.show_all_days()
-        return self.days_table_template(days_details)
+    def show_all_daily_summaries_per_month(self):
+        try:
+            month = int(input("MONTH (number): "))
+            year = int(input("YEAR (number): "))
+            days_details = self.db.show_daily_summaries_per_month(month, year)
+            return self.days_table_template(days_details)
+        except Exception as e:
+            print(e)
+
+    def show_monthly_summary(self):
+        try:
+            month = int(input("MONTH (number): "))
+            year = int(input("YEAR (number): "))
+            check_that_summary_exist = self.db.check_that_monthly_summary_exist(month, year)
+            if check_that_summary_exist == False:
+                self.db.add_monthly_summary(month, year)
+            days_details_list = self.db.show_daily_summaries_per_month(month, year)
+            working_days = len(days_details_list)
+            total_hours = self.calculate_total_hours(days_details_list)
+            average_hours_per_day = self.calculate_average_hours_per_day(days_details_list)
+            kilometers = sum([i[7] for i in days_details_list])
+            refuels = sum([i[8] for i in days_details_list])
+            fuel_standard = round(refuels * 100 / kilometers)
+            difference = sum([i[10] for i in days_details_list])
+            salary = self.calculate_salary(daily_rate, working_days, hourly_rate, total_hours)
+            self.db.update_monthly_summary(month, year, working_days, total_hours, average_hours_per_day,
+                                           kilometers, refuels, fuel_standard, difference, salary)
+            monthly_summary = self.db.show_monthly_summary(month, year)
+            return self.month_table_template(monthly_summary)
+        except Exception as e:
+            print(e)
 
     def calculate_hours(self, start_hour_string, end_hour_string):
         hours_format = "%H:%M"
@@ -49,6 +112,23 @@ class App:
         end_hour = datetime.strptime(end_hour_string, hours_format)
         difference = end_hour - start_hour
         return str(difference)
+
+    def calculate_total_hours(self, details_list):
+        total_seconds_list = [self.time_to_seconds(i[6]) for i in details_list]
+        total_seconds = sum(total_seconds_list)
+        total_hours = total_seconds / 3600
+        return str(total_hours)
+
+    def calculate_average_hours_per_day(self, details_list):
+        days = len(details_list)
+        total_seconds_list = [self.time_to_seconds(i[6]) for i in details_list]
+        total_seconds = sum(total_seconds_list)
+        average_hours_per_day = total_seconds / 3600 / days
+        return str(average_hours_per_day)
+
+    def time_to_seconds(self, time_str):
+        h, m, s = map(int, time_str.split(':'))
+        return h * 3600 + m * 60 + s
 
     def calculate_standard_fuel_usage(self, kilometers):
         standard_usage = kilometers * 0.25
@@ -58,6 +138,10 @@ class App:
         difference = standard_fuel_usage - refuel
         return difference
 
+    def calculate_salary(self, daily_rate, working_days, hourly_rate, total_hours):
+        salary = (daily_rate * working_days) + (hourly_rate * total_hours)
+        return round(salary, 2)
+
     def start(self):
         self.is_running = True
         print("Hello !")
@@ -66,10 +150,12 @@ class App:
             try:
                 command_input = input("TYPE: ")
                 if command_input == "1":
-                    self.add_new_detail()
+                    self.add_new_daily_summary()
                 elif command_input == "2":
-                    self.show_all_days_details()
+                    self.show_all_daily_summaries_per_month()
                 elif command_input == "3":
+                    self.show_monthly_summary()
+                elif command_input == "4":
                     print("Bye !")
                     self.is_running = False
                 else:
